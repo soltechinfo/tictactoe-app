ae58143dc59ed5c1ecebb8d77e16fbb29554fd9c
soltechinfo <soltech@soltech.co.kr>
2023-12-05 11:44:17 +0900

    2층: 패컴 TicTacToe 05 강 까지

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        game-board
+      </div>
+      <dev className="game-info">
+        game-info
+      </dev>
     </div>
   );
 }
